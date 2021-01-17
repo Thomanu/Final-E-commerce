90004804523f5c894869ecd6268528b50890a2d0
thomas.bolot <tbolot@gmail.com>
2021-01-17 19:11:56 +0100

    [+] Mon projet e-commerce

---
+++ assets/database.js
@@ -0,0 +1,189 @@
+var product= [{
+    "id": 0,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Bose Quietcomfort II",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+},
+{
+    "id": 1,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Marshall Monitor II ANC",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+},
+{
+    "id": 2,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Bose Headphones 700",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+},
+{
+
+    "id": 3,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Steelseries Artcis Pro",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+},
+{
+    "id": 4,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Logitech 952 Lightspeed",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+},{
+
+    "id": 5,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Shangri-la Hifi",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+},{
+
+    "id": 6,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Jabra Elite 85h",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+}, {
+
+    "id": 7,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Sony XMH301",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+}, {
+    "id": 8,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Corsair h35",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+}, {
+    "id": 9,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Beats Powerbeat",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+},
+{
+    "id": 10,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Kraken Gaming",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+}, {
+    "id": 11,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Logitech G35",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+},{
+
+    "id": 12,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Powerbeats True Wireless",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+},{
+
+    "id": 13,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Seinheiser Wireless",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+},{
+
+    "id": 14,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Audeze Lcd",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+},{
+
+    "id": 15,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Be quiet Headset",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+},{
+
+    "id": 16,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Abyss Diana Hifi",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+},{
+
+    "id": 17,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Focal Elear",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+},{
+
+    "id": 19,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "HifiMan Susvara",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+},
+{
+    "id": 20,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Audeze lcd gx ",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+},
+{
+    "id": 21,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Neumann NDH",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+},{
+
+    "id": 22,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Bowers & Wilkins Signature",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+},{
+
+    "id": 23,
+    "img": "assets/img/vetements/homme/hmgoepprod (22).jpg",
+    "nom": "Focal Jmlab",
+    "price": 39.99,
+    "desc": "",
+    "classname": "pants 50 bleu"
+
+
+
+
+
+
+}]
+
